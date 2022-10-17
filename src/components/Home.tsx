d93df831f83fb178debf590e8ec4f4105b96c757
Nacho <nachomocoroa@gmail.com>
2022-10-17 04:28:29 +0200

    [FEATURE] Home section design

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,19 +8,40 @@ const Home = () => {
 
     const userDetails: AuthStateProps = useAuthState();
     const isAuthenticated: boolean = Boolean(userDetails.token);
+
+    const wrapperStyle = {
+        backgroundImage: `url('https://mdbcdn.b-cdn.net/img/new/slides/041.webp')`, 
+        backgroundRepeat: 'no-repeat', 
+        backgroundPosition: 'center', 
+        backgroundSize: 'cover' 
+    };
+
+    const maskStyle = {
+        backgroundColor: `rgba(0, 0, 0, 0.6)`
+    };
     
     return (
-        <div>
-            <div>HOME</div>
-            {
-                isAuthenticated ? 
-                    (
-                        <Link to={NODES} className="btn btn-outline-light btn-lg">Private section</Link>
-                    ) : (
-                        <Link to={LOGIN} className="btn btn-outline-light btn-lg">Log in</Link>
-                    )
-            }
-        </div>
+        <section 
+            className="w-100 vh-100 d-flex flex-column justify-content-center" 
+            style={wrapperStyle}
+        >
+            <div
+                className="mask p-5 h-75" 
+                style={maskStyle}
+            >
+                <div className="d-flex justify-content-center align-items-center h-100">
+                    <div className="text-white text-center">
+                        <h1 className="mb-3">Welcome to NodesApp</h1>
+                        <h4 className="mb-3">Please continue to Login/ Register.</h4>
+                        { isAuthenticated ? (
+                            <Link to={NODES} className="btn btn-outline-light btn-lg">Private section</Link>
+                        ) : (
+                            <Link to={LOGIN} className="btn btn-outline-light btn-lg">Log in</Link>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
