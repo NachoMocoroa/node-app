4c3661f8e531a48f771bb4b0f5a89264584dc34c
Nacho <nachomocoroa@gmail.com>
2022-10-17 06:31:47 +0200

    [FEATURE] Suspense and lazy implementation for data fetch and components load

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,30 @@ import { LOGIN, LOGOUT, NODES } from './Routes/paths';
 import { AuthProvider } from './contexts/auth';
 import { NodesProvider } from './contexts/nodes';
 
-import PrivateRoute from './Routes/router/PrivateRoute';
-import PublicRoute from './Routes/router/PublicRoute';
-import Home from './components/Home';
-import Login from './components/Login';
-import Logout from './components/Logout';
-import Nodes from './components/nodes/Nodes';
+const PrivateRoute = React.lazy(() => import('./Routes/router/PrivateRoute'));
+const PublicRoute = React.lazy(() => import('./Routes/router/PublicRoute'));
+const Home = React.lazy(() => import('./components/Home'));
+const Login = React.lazy(() => import('./components/Login'));
+const Logout = React.lazy(() => import('./components/Logout'));
+const Nodes = React.lazy(() => import('./components/nodes/Nodes'));
+const Spinner = React.lazy(() => import('./components/Spinner'));
 
 const App = () => (
 	<AuthProvider>
 		<NodesProvider>
 			<BrowserRouter>
-				<Routes>
-					<Route path={NODES} element={<PrivateRoute />}>
-						<Route path={NODES} element={<Nodes />} />
-						<Route path={LOGOUT} element={<Logout />} />
-					</Route>
-					<Route path="/" element={<PublicRoute />}>
-						<Route index element={<Home />} />
-						<Route path={LOGIN} element={<Login />} />
-					</Route>
-				</Routes>
+        		<Suspense fallback={<Spinner />}>
+					<Routes>
+						<Route path={NODES} element={<PrivateRoute />}>
+							<Route path={NODES} element={<Nodes />} />
+							<Route path={LOGOUT} element={<Logout />} />
+						</Route>
+						<Route path="/" element={<PublicRoute />}>
+							<Route index element={<Home />} />
+							<Route path={LOGIN} element={<Login />} />
+						</Route>
+					</Routes>
+				</Suspense>
 			</BrowserRouter>
 		</NodesProvider>
 	</AuthProvider>
